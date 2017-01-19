588c23e30c6893706b9e8992fddda0ced6fa224a
Garity Chapman <garitychapman@gmail.com>
2017-01-19 10:51:58 -0500

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ImagePickerIOS,
+  Image
+} from 'react-native';
+
+
+export default class lilBean extends Component {
+  constructor() {
+    super();
+    this.state = { image: null };
+  }
+
+  componentDidMount() {
+    this.pickImage();
+  }
+
+  pickImage() {
+    // openSelectDialog(config, successCallback, errorCallback);
+    ImagePickerIOS.openSelectDialog({}, imageUri => {
+      this.setState({ image: imageUri });
+    }, error => console.error(error));
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        {this.state.image?
+          <Image style={{ flex: 1 }} source={{ uri: this.state.image }} /> :
+          null
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('lilBean', () => lilBean);
